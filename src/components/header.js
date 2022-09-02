00b6f00c03ee48724d19676a4c3f0a0f3ac2c1d0
Jesus <jesusdavidgonzales1102@gmail.com>
2022-09-02 15:53:51 -0500

    Commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <>
+            <h1>Hola estamos creando el portafolio de jesus</h1>
+        </>
+    )
+}
+
+export default Header

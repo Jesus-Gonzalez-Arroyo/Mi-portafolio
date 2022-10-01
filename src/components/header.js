03c0ed0cfffcfbdb90e9122864003aa460106663
Jesus <jesusdavidgonzales1102@gmail.com>
2022-10-01 14:29:02 -0500

    Mejoras

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,25 @@
 import React from "react";
 import './style/header.css'
+import {FaBars} from 'react-icons/fa'
 
-class Header extends React.Component{
-    render(){
-        return(
-            <div className="container-header">
-                <div className="container-menu">
-                    <ul className="ul">
-                        <li className="li"><a href="#conocimiento">Conocimientos</a></li>
-                        <li className="li"><a href="#proyectos">Proyectos</a></li>
-                        <li className="li"><a href="#contacto">Contacto</a></li>
-                    </ul>
-                </div>
-            </div>
-        )
+export default function Header(){
+
+    function menu(){
+        
     }
-}
 
-export default Header
+    return(
+        <div className="container-header">
+            <div className="container-icon-menu">
+                <FaBars className="icon-menu" onClick={()=> menu()}/>
+            </div>
+            <div className="container-menu">
+                <ul className="ul">
+                    <li className="li"><a href="#conocimiento">Conocimientos</a></li>
+                    <li className="li"><a href="#proyectos">Proyectos</a></li>
+                    <li className="li"><a href="#contacto">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/menu_despegable.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Menudespegable(){
+    return(
+        <div className="container-menu" id="menu_despegable">
+            <div className="items">
+                <nav>
+                    <li>
+                        <a href="#">Conocimientos</a>
+                    </li>
+                    <li>
+                        <a href="#">Proyectos</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </nav>
+            </div> 
+        </div>
+    )
+}
+
+
+
+
+
+
+

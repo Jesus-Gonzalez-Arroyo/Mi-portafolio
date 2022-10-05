0f48253da42b6a843c41e2aa517c2117dec2ddbc
Jesus <jesusdavidgonzales1102@gmail.com>
2022-10-05 11:52:24 -0500

    Nuevas funciones

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -5,6 +5,7 @@ import Conocimientos from "../components/conocimientos";
 import Proyectos from "../components/proyectos";
 import Contacto from "../components/contacto";
 import Menudespegable from "../components/menu_despegable";
+import Burbujas from "../components/burbujas";
 
 class App extends React.Component{
     render(){
@@ -21,7 +22,8 @@ class App extends React.Component{
                 </main>
                 <footer>
                     <Contacto/>
-                </footer>   
+                </footer>
+                <Burbujas/>  
             </>
         )
     }
---
+++ src/components/burbujas.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './style/burbujas.css'
+
+
+export default function Burbujas(){
+    return(
+        <div className="container-burbujas">
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+            <div className="burbujas"></div>
+        </div>
+    )
+}

d78ff8989f7f8847c18892a7300e62d63b99f0f7
Jesus <jesusdavidgonzales1102@gmail.com>
2023-02-25 11:32:12 -0500

    Añadiendo componente aboutme

---
+++ src/components/Aboutme.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../components/style/aboutme.css'
+
+export default function Aboutme(){
+    return(
+        <div className='container_aboutme'>
+            <span className='pincho1'></span>
+            <span className='pincho2'></span>
+            <p className='title_aboutme'>Sobre mi</p>
+            <p className='description_aboutme'>Tengo estudios en analista y desarrollador de sistemas de informacion, ademas de eso formacion en algunos bootcamps.<br/> Me gusta mucho el mundo de la tecnologia, por lo que hago mi trabajo con mucho amor y dedicacion.<br/> Siempre estoy en constante aprendizaje para ampliar mis conocimientos y ponerme nuevos retos.</p>
+        </div>
+    )
+}
